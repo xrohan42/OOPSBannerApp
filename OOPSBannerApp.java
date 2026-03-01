c0396186b402409ffcf323b4bf07355e2d8a501a
xrohan42 <xrohan42@gmail.com>
2026-03-01 22:50:32 +0530

    UC3: Used String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,16 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        System.out.println(" ***   ***   ****   **** ");
-        System.out.println("*   * *   *  *   *  *    ");
-        System.out.println("*   * *   *  ****   **** ");
-        System.out.println("*   * *   *  *         * ");
-        System.out.println(" ***   ***   *      **** ");
+        String[] line1 = {" ***  ", " ***  ", " **** ", " **** "};
+        String[] line2 = {"*   * ", "*   * ", "*   * ", " *    "};
+        String[] line3 = {"*   * ", "*   * ", "****  ", " **** "};
+        String[] line4 = {"*   * ", "*   * ", "*     ", "    * "};
+        String[] line5 = {" ***  ", " ***  ", "*     ", " **** "};
+
+        System.out.println(String.join(" ", line1));
+        System.out.println(String.join(" ", line2));
+        System.out.println(String.join(" ", line3));
+        System.out.println(String.join(" ", line4));
+        System.out.println(String.join(" ", line5));
     }
 }
