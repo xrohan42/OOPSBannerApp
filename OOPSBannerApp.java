06a92b4d727f481f547568558c3b987c7fba5e34
xrohan42 <xrohan42@gmail.com>
2026-03-01 23:08:34 +0530

    UC6: Refactored using helper methods for patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,48 @@
 public class OOPSBannerApp {
+
+    public static String[] getOPattern() {
+        return new String[]{
+                " ***  ",
+                "*   * ",
+                "*   * ",
+                "*   * ",
+                " ***  "
+        };
+    }
+
+
+    public static String[] getPPattern() {
+        return new String[]{
+                " **** ",
+                " *  * ",
+                " **** ",
+                " *    ",
+                " *    "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                " **** ",
+                " *    ",
+                " **** ",
+                "    * ",
+                " **** "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] banner = {
-                String.join(" ", " ***  ", " ***  ", " **** ", " **** "),
-                String.join(" ", "*   * ", "*   * ", "*   * ", " *    "),
-                String.join(" ", "*   * ", "*   * ", "****  ", " **** "),
-                String.join(" ", "*   * ", "*   * ", "*     ", "    * "),
-                String.join(" ", " ***  ", " ***  ", "*     ", " **** ")
-        };
+        String[] o1 = getOPattern();
+        String[] o2 = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-        for (String line : banner) {
-            System.out.println(line);
+        // Loop through each row
+        for (int i = 0; i < o1.length; i++) {
+            System.out.println(String.join(" ",
+                    o1[i], o2[i], p[i], s[i]
+            ));
         }
     }
 }
